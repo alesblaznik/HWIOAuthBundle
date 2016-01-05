d894805d89bd6013ecb4ee5cd481f4723d78498c
Ales Blaznik <ales.blaznik@dlabs.si>
2016-01-05 17:02:25 +0000

    Override getAccessToken method

--- OAuth/ResourceOwner/VisualStudioResourceOwner.php
+++ OAuth/ResourceOwner/VisualStudioResourceOwner.php
@@ -8,6 +8,7 @@
 
 namespace HWI\Bundle\OAuthBundle\OAuth\ResourceOwner;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
@@ -17,12 +18,18 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
  */
 class VisualStudioResourceOwner extends GenericOAuth2ResourceOwner
 {
+    /**
+     * {@inheritDoc}
+     */
     protected $paths = [
         'identifier'     => 'id',
         'nickname'       => 'emailAddress',
         'realname'       => 'displayName',
     ];
 
+    /**
+     * {@inheritDoc}
+     */
     protected function configureOptions(OptionsResolverInterface $resolver)
     {
         parent::configureOptions($resolver);
@@ -33,5 +40,26 @@ class VisualStudioResourceOwner extends GenericOAuth2ResourceOwner
             'infos_url'         => 'https://app.vssps.visualstudio.com/_apis/profile/profiles/me?api-version=1.0',
             'attr_name'         => 'assertion',
         ]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAccessToken(Request $request, $redirectUri, array $extraParameters = array())
+    {
+        $parameters = array_merge(array(
+            'assertion'             => $request->query->get('code'),
+            'grant_type'            => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+            'client_assertion_type' => 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+            'client_assertion'      => $this->options['client_secret'],
+            'redirect_uri'          => $redirectUri,
+        ), $extraParameters);
+
+        $response = $this->doGetTokenRequest($this->options['access_token_url'], $parameters);
+        $response = $this->getResponseContent($response);
+
+        $this->validateResponseContent($response);
+
+        return $response;
     }
 }
