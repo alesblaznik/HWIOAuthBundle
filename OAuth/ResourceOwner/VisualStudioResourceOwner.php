be46a41b79b7032d20b846357963f0b6acaf28e6
Ales Blaznik <ales.blaznik@dlabs.si>
2019-01-04 15:31:51 +0100

    Use concrete OptionsResolver, not interface

--- OAuth/ResourceOwner/VisualStudioResourceOwner.php
+++ OAuth/ResourceOwner/VisualStudioResourceOwner.php
@@ -9,7 +9,7 @@
 namespace HWI\Bundle\OAuthBundle\OAuth\ResourceOwner;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Class VisualStudioResourceOwner
@@ -30,7 +30,7 @@ class VisualStudioResourceOwner extends GenericOAuth2ResourceOwner
     /**
      * {@inheritDoc}
      */
-    protected function configureOptions(OptionsResolverInterface $resolver)
+    protected function configureOptions(OptionsResolver $resolver)
     {
         parent::configureOptions($resolver);
 
